9929457d30f3c6d1bb7df81b75f663b29afccc82
Justin <justin.koestinger@edu.hefr.ch>
2017-02-28 16:04:01 +0100

    The commit that will change the world forever.

--- code/api.py
+++ code/api.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
+@app.route('/bye')
+def bye():
+    return "Good bye, cruel world !"
+
 if __name__ == '__main__':
     app.run(debug=True)
