118b7051d8e704cf9b05f8a6221fb1e38e5275ab
RicardoPortela <ricardo.portela@edu.hefr.ch>
2017-02-28 16:23:24 +0100

    Specified the Hello World to say that we come from planet Earth

--- code/api.py
+++ code/api.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return "Hello, World! I'm from the planet Earth! o/"
 
 if __name__ == '__main__':
     app.run(debug=True)
